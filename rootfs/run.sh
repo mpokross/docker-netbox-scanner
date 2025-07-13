e7708b85bff6cf823a3488cf0d5df9aefc2b9fe1
mpokross <mpokross@gmail.com>
2025-07-12 23:31:01 -0400

    Update run.sh

--- rootfs/run.sh
+++ rootfs/run.sh
@@ -8,7 +8,7 @@ if [ ! -s /netbox-scanner/networks.txt ]; then
 	exit 1
 fi >&2
 
-if echo '4de64ad74607f128bdb5873497a9d85d27e52c0a96dc994016488d050a55dd6c  /netbox-scanner.conf' | sha256sum --check --status; then
+if echo '4de64ad74607f128bdb5873497a9d85d27e52c0a96dc994016488d050a55dd6c  /root/.netbox-scanner.conf' | sha256sum --check --status; then
 	echo "Error: You have to configure at least ADDRESS and TOKEN in 'netbox-scanner.conf'."
 	echo
 	exit 1
